2736e496f143d332a7eff76dd01c968382f9493e
ferreira-gn <geanoficial14@gmail.com>
2026-06-11 10:58:12 -0300

    chore: add nativeWind lib to the project

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,20 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { CirclePlus } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const home: React.FC = () => {
   return (
-    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Hello world</Text>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <View className="flex flex-1 items-center justify-center bg-gray-500">
+        <Text className="text-xl font-bold text-white-500">Welcome to Nativewind!</Text>
+
+        <TouchableOpacity className="bg-black flex flex-row gap-2 p-4 rounded my-2">
+          <Text>Touch Me</Text>
+          <HugeiconsIcon icon={CirclePlus} color={"#44A5AA"} size={18} opacity={1} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaProvider>
   );
 };
 

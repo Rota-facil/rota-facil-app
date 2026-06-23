ad504b87717c35aefa068fe1c560c0fcb82443ea
ferreira-gn <geanoficial14@gmail.com>
2026-06-23 11:07:45 -0300

    feat: implement overview flow

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,26 @@
 import { CirclePlus } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react-native";
+import { Redirect } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import OnboardingScreen from "./overview/screens/OverviewScreen";
+import { useStorage } from "../hooks/useStorage";
+import OnboardingScreen from "../presentation/shared/screens/OverviewScreen";
 
 const home: React.FC = () => {
+  const { token, firstAccess, isLoading, error } = useStorage();
+
+  if (isLoading) {
+    // screen loading
+  }
+
+  if (token) {
+    // return <Redirect href="/home" />;
+  }
+
+  if (!firstAccess && firstAccess !== null) {
+    return <Redirect href="/login" />;
+  }
+
   return (
     <SafeAreaProvider>
       <OnboardingScreen />

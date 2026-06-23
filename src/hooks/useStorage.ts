c4aaa4d1b26ec3d764825491b24e1e085e0ce153
ferreira-gn <geanoficial14@gmail.com>
2026-06-23 11:01:17 -0300

    feat: create service and hook to storage tokens

---
+++ src/hooks/useStorage.ts
@@ -0,0 +1,37 @@
+import { useCallback, useEffect, useState } from "react";
+import { StorageError } from "@/core/errors/storageError";
+import { STORAGE_KEYS, storageService } from "../core/service/storageService";
+
+export function useStorage() {
+  const [token, setToken] = useState<string | null>(null);
+  const [firstAccess, setFirstAccess] = useState<boolean | null>(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<StorageError | null>(null);
+
+  const getToken = useCallback(async () => {
+    try {
+      const data = await storageService.get<string>(STORAGE_KEYS.AUTH_TOKEN);
+      setToken(data);
+    } catch (err) {
+      const storageError = err instanceof StorageError ? err : new StorageError(String(err));
+      setError(storageError);
+    }
+  }, []);
+
+  const getFirstAccess = useCallback(async () => {
+    try {
+      const data = await storageService.get<boolean>(STORAGE_KEYS.FIRST_ACCESS);
+      setFirstAccess(data);
+    } catch (err) {
+      const storageError = err instanceof StorageError ? err : new StorageError(String(err));
+      setError(storageError);
+    }
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    Promise.all([getToken(), getFirstAccess()]).finally(() => setIsLoading(false));
+  }, [getToken, getFirstAccess]);
+
+  return { token, firstAccess, isLoading, error };
+}

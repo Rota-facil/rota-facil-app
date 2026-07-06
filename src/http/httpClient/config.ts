0cb5e80268e900dad4f7b1a304cab4c4601fc0b4
ferreira-gn <geanoficial14@gmail.com>
2026-07-06 08:17:18 -0300

    refactor: change api base url to environment variables

--- src/http/httpClient/config.ts
+++ src/http/httpClient/config.ts
@@ -2,5 +2,5 @@
  * Configuração central de ambiente.
  */
 export const config = {
-  apiBaseUrl: "http://localhost:8080", // url de desenvolvimento
+  apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
 } as const;

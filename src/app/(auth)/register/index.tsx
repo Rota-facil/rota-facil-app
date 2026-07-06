392ce2207ffcb12004f351c7fd5b97ad4f778991
ferreira-gn <geanoficial14@gmail.com>
2026-07-06 09:35:55 -0300

    feat: wire authentication and private app routes

---
+++ src/app/(auth)/register/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function RegisterPage() {
+  return (
+    <View className="flex flex-1 items-center justify-center">
+      <Text className="text-lime-800">Hello register page</Text>
+    </View>
+  );
+}

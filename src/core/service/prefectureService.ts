da13e58c23a6cd651263ca5703f66ad8a52fa835
ferreira-gn <geanoficial14@gmail.com>
2026-07-06 16:17:24 -0300

    fix: load prefectures without access token

--- src/core/service/prefectureService.ts
+++ src/core/service/prefectureService.ts
@@ -1,32 +1,18 @@
-import { SoftError } from "@/errors/errors";
 import { PrefectureRequest } from "@/http/request/prefectureRequest";
 import type { PrefectureEntity } from "../entity/prefectureEntity";
 import { Mapper } from "../mappers/mappers";
-import { STORAGE_KEYS, StorageService } from "./storageService";
 
 const PrefectureService = {
   async getPrefectures(): Promise<PrefectureEntity[]> {
-    const accessToken = await this.getAccessToken();
-    const dtos = await PrefectureRequest.getPrefectures(accessToken);
+    const dtos = await PrefectureRequest.getPrefectures();
 
     return Mapper.prefecture.toEntityList(dtos);
   },
 
   async getPrefecture(prefectureId: string): Promise<PrefectureEntity> {
-    const accessToken = await this.getAccessToken();
-    const dto = await PrefectureRequest.getPrefecture(accessToken, prefectureId);
+    const dto = await PrefectureRequest.getPrefecture(prefectureId);
 
     return Mapper.prefecture.toEntity(dto);
-  },
-
-  async getAccessToken(): Promise<string> {
-    const accessToken = await StorageService.get<string>(STORAGE_KEYS.AUTH_TOKEN);
-
-    if (!accessToken) {
-      throw new SoftError("Sessão não encontrada. Faça login novamente.");
-    }
-
-    return accessToken;
   },
 };
 
--- src/http/request/prefectureRequest.ts
+++ src/http/request/prefectureRequest.ts
@@ -2,18 +2,12 @@ import type { PrefectureResponseDTO } from "../dto/prefectureDTO";
 import { httpClient } from "../httpClient/httpClient";
 
 const PrefectureRequest = {
-  async getPrefectures(accessToken: string): Promise<PrefectureResponseDTO[]> {
-    console.log("getPrefectures", accessToken);
-    return httpClient.get<PrefectureResponseDTO[]>("/auth/prefectures", {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
+  async getPrefectures(): Promise<PrefectureResponseDTO[]> {
+    return httpClient.get<PrefectureResponseDTO[]>("/auth/prefectures");
   },
 
-  async getPrefecture(accessToken: string, prefectureId: string): Promise<PrefectureResponseDTO> {
-    console.log("getPrefecture", accessToken, prefectureId);
-    return httpClient.get<PrefectureResponseDTO>(`/auth/prefectures/${prefectureId}`, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
+  async getPrefecture(prefectureId: string): Promise<PrefectureResponseDTO> {
+    return httpClient.get<PrefectureResponseDTO>(`/auth/prefectures/${prefectureId}`);
   },
 };
 

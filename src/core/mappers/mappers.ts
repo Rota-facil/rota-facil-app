aa40fb67cc39fb99422031e04e751cc80502d073
ferreira-gn <geanoficial14@gmail.com>
2026-07-07 17:39:44 -0300

    feat: add bus data flow

--- src/core/mappers/mappers.ts
+++ src/core/mappers/mappers.ts
@@ -1,4 +1,5 @@
 import { authMapper } from "./authMapper";
+import { busMapper } from "./busMapper";
 import { prefectureMapper } from "./prefectureMapper";
 import { userMapper } from "./userMapper";
 
@@ -6,6 +7,7 @@ const Mapper = {
   user: userMapper,
   auth: authMapper,
   prefecture: prefectureMapper,
+  bus: busMapper,
 } as const;
 
 export { Mapper };

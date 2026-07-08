fcc87925899316dde0d7e59152581d0f5a365ec1
ferreira-gn <geanoficial14@gmail.com>
2026-07-08 12:15:31 -0300

    feat: route private home by user role

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
-import { Redirect } from "expo-router";
+import { type Href, Redirect } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { useSession } from "@/hooks/useSession";
 import OnboardingScreen from "../presentation/shared/screens/OverviewScreen";
+
+const privateRoute = "/(private)" as Href;
 
 const home: React.FC = () => {
   const { loading, session, firstAccess } = useSession();
@@ -11,7 +13,7 @@ const home: React.FC = () => {
   }
 
   if (session) {
-    return <Redirect href="/(private)/students/home" />;
+    return <Redirect href={privateRoute} />;
   }
 
   if (!firstAccess) {

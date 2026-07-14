8ffc64a9e0ee9081bcda5b4e17dd9849d437f4f5
ferreira-gn <geanoficial14@gmail.com>
2026-07-14 01:45:32 -0300

    fix: remove unecessary environment variables

--- src/http/httpClient/config.ts
+++ src/http/httpClient/config.ts
@@ -3,6 +3,5 @@
  */
 export const config = {
   apiBaseUrl: process.env.EXPO_PUBLIC_API_BASE_URL,
-  apiBaseUrlGoogle: process.env.EXPO_PUBLIC_API_BASE_URL_GOOGLE,
   osrmBaseUrl: process.env.EXPO_PUBLIC_OSRM_BASE_URL ?? "https://router.project-osrm.org",
 } as const;
